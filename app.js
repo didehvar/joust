0e9125efcf97545002e777a8c2d86f0d305707f8
James Didehvar <james@didehvar.com>
2014-04-13 17:04:26 +0100

    remove stylus/nib, will use less

--- app.js
+++ app.js
@@ -9,10 +9,6 @@ var url = require('url');
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/joust');
 
-// improve css modules
-var stylus = require('stylus');
-var nib = require('nib');
-
 // set environment
 var env = process.env.NODE_ENV || 'development';
 
@@ -66,23 +62,5 @@ app.get('/auth/steam/callback',
     res.redirect('/');
   }
 );
-
-// development only configuration
-if (env === 'development') {
-  // configure stylus compilation from assets to public
-  app.use(stylus.middleware({
-    src: path.join(__dirname, 'assets'),
-    dest: path.join(__dirname, 'public'),
-    force: true,
-    compress: true,
-    compile: function(str, path) {
-      return stylus(str)
-        .set('filename', path)
-        .set('compress', true)
-        .use(nib())
-        .import('nib');
-    }
-  }));
-}
 
 module.exports = app;

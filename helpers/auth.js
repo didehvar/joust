3928ef45ad31a6dab92c04ba01b7cc500f120993
James Didehvar <james@didehvar.com>
2014-04-14 00:51:29 +0100

    grab steam name and avatars on login

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,7 +1,38 @@
 var passport = require('passport');
-var steam_strategy = require('passport-steam').Strategy;
-var User = require('../models/users');
+var steam_web = require('steam-web');
+var openid_strategy = require('passport-openid').Strategy;
+var User = require('../models/user');
 var url = require('url');
+
+function update_steam_data(steamid, user, done) {
+  var steam = new steam_web({
+    apiKey: process.env.JOUST_STEAM_KEY,
+    format: 'json'
+  });
+
+  steam.getPlayerSummaries({
+    steamids: [steamid],
+    callback: function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      var profile = result.response.players[0];
+
+      // user has been created by this point, proceed to update their steam info
+      user.display_name = profile.personaname;
+      user.avatar = profile.avatar;
+      user.avatar_medium = profile.avatarmedium;
+      user.avatar_full = profile.avatarfull;
+
+      user.save(function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+  });
+}
 
 module.exports = function(app) {
   passport.serializeUser(function(user, done) {
@@ -14,20 +45,26 @@ module.exports = function(app) {
     });
   });
 
-  passport.use(new steam_strategy({
+  passport.use(new openid_strategy({
       returnURL: process.env.AUTH_RETURN_URL || 'http://localhost:8111/auth/steam/callback',
       realm: process.env.AUTH_REALM || 'http://localhost:8111',
-      apiKey: process.env.JOUST_STEAM_KEY
+      providerURL: 'http://steamcommunity.com/openid',
+      profile: true,
+      stateless: true
     },
-    function(identifier, profile, done) {
+    function(identifier, done) {
       process.nextTick(function() {
+        // the returned identifier is a url to the steam user, e.g.
+        // http://steamcommunity.com/openid/id/12345678912345678
         var steamid = url.parse(identifier).pathname.split('/').pop();
+
         User.findOne({ steamid: steamid }, function(err, user) {
           if (err) {
             return done(err);
           }
 
           if (user) {
+            update_steam_data(steamid, user, done);
             return done(err, user);
           } else {
             var new_user = new User();
@@ -40,6 +77,7 @@ module.exports = function(app) {
                 throw err;
               }
 
+              update_steam_data(steamid, new_user, done);
               return done(null, new_user);
             });
           }
@@ -53,14 +91,14 @@ module.exports = function(app) {
 
   // cannot use paspport.authenticate with express-path (?)
   app.get('/auth/steam',
-    passport.authenticate('steam'),
+    passport.authenticate('openid'),
     function(req, res, next) {
       // function not called
     }
   );
 
   app.get('/auth/steam/callback',
-    passport.authenticate('steam', {
+    passport.authenticate('openid', {
       successRedirect: '/',
       failureRedirect: '/auth/steam/failed',
       failureFlash: true,
--- models/user.js
+++ models/user.js
@@ -2,6 +2,10 @@ var mongoose = require('mongoose');
 
 var user_schema = mongoose.Schema({
   steamid: Number,
+  display_name: String,
+  avatar: String, // 32x32
+  avatar_medium: String, // 64x64
+  avatar_full: String, // 184x184
   created: Date
 });
 

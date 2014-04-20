5255d8578f25800a9b0d6013413e626a3268935d
didehvar <james@didehvar.com>
2014-04-20 12:54:14 +0100

    oops

--- helpers/utility.js
+++ helpers/utility.js
@@ -2,5 +2,5 @@
 exports.number = function(obj) {
   // praise be to the holy stack overflow lords
   // http://stackoverflow.com/questions/1303646/check-whether-variable-is-number-or-string-in-javascript
-  return !isNaN(num - 0) && num !== null && num.replace(/^\s\s*/, '') !== '' && num !== false;
+  return !isNaN(obj - 0) && obj !== null && obj.replace(/^\s\s*/, '') !== '' && obj !== false;
 }

420b7abe6b0046e12a9991d9f1161d84b3eed718
didehvar <james@didehvar.com>
2014-04-20 01:25:39 +0100

    mmm comments

--- controllers/user.js
+++ controllers/user.js
@@ -1,23 +1,33 @@
+/**
+ * All routes to do with user actions and pages.
+ * 
+ * author: @didehvar
+ * version: 0.0.1
+ */
+
 var url = require('url');
 var User = require('../models/user');
 
+// passes a user to the login route after setting the return url.
 exports.login = function(req, res) {
   req.session.returnTo = url.parse(req.url, true).query.return;
   res.redirect('/auth/steam');
 };
 
+// logs the user out and redirects them back to the homepage.
 exports.logout = function(req, res) {
   req.logout();
   res.redirect('/');
 };
 
+// displays the profile page for the specified user.
 exports.profile = function(req, res, next) {
   var id = req.params.id;
 
   var find_user = function(field) {
     var arr = {};
     arr[field] = id;
-    
+
     User.findOne(arr, function(err, user) {
       if (err || user === null) {
         return next();

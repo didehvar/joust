d28cbc23b74370883768f9b21c131ace73c4233f
didehvar <james@didehvar.com>
2014-04-21 17:32:15 +0100

    pre-break

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var env = process.env.NODE_ENV || 'development';
 
 // compile less files
 app.use(require('less-middleware')(path.join(__dirname, 'assets', 'less'), {
-  //debug: env === 'development' ? true : false,
   dest: path.join(__dirname, 'public'),
   force: env === 'development' ? true : false,
   preprocess: {
@@ -43,12 +42,14 @@ require('./helpers/session')(app);
 // passport setup
 require('./helpers/auth')(app);
 
+// needed to parse post requests
 app.use(require('body-parser')());
 
 // helper functions available in templates
 app.locals.basedir = path.join(__dirname, 'views');
 app.locals.inflection = require('inflection');
 
+// variables accessible in views
 app.use(function(req, res, next) {
   res.locals.url = req.url;
   res.locals.user = req.user;
@@ -57,7 +58,7 @@ app.use(function(req, res, next) {
 });
 
 // app routes
-var routes = [
+require('express-path')(app, [
   ['/*', 'index#fix_www'],
   ['/', 'index#index'],
 
@@ -72,43 +73,44 @@ var routes = [
   ['/users/:id', 'api/user#find', 'get'],
   ['/users/:id', 'api/user#update', 'put'],
   ['/users/:id', 'api/user#delete', 'delete']
-];
-
-require('express-path')(app, routes);
+]);
 
 // 404 handler
 app.use(function(req, res, next) {
   res.status(404);
 
-  if (req.accepts('html')) {
-    res.render('error', { message: 'Page not found', url: req.url });
-    return;
-  }
+  res.format({
+    text: function() {
+      res.type('txt').send('Not found');
+    },
 
-  if (req.accepts('json')) {
-    res.send({ error: 'Not found' });
-    return;
-  }
+    html: function() {
+      res.render('error', { message: 'Page not found', url: req.url });
+    },
 
-  res.type('txt').send('Not found');
+    json: function() {
+      res.send({ error: 'Not found!' });
+    }
+  });
 });
 
 // error handler
 app.use(function(err, req, res, next) {
   res.status(500);
 
-  if (req.accepts('html')) {
-    res.render('error', { error: err });
-    return;
-  }
+  res.format({
+    text: function() {
+      res.type('txt').send(err);
+    },
 
-  if (req.accepts('json')) {
-    res.send({ error: err });
-    return;
-  }
+    html: function() {
+      res.render('error', { error: err });
+    },
 
-  res.type('txt').send(err);
-
+    json: function() {
+      res.send({ error: err });
+    }
+  });
 });
 
 module.exports = app;

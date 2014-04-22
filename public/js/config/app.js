d05f9cf6ab478c98895016fa394a8e63c9263dab
James Didehvar <james@didehvar.com>
2014-04-22 17:22:38 +0100

    don't think ember data is needed

--- public/js/config/app.js
+++ public/js/config/app.js
@@ -1,14 +1,12 @@
-require('../../bower_components/jquery/dist/jquery');
+require('../../bower_components/jquery/dist/jquery.js');
 
-require('../../bower_components/handlebars/handlebars');
-require('../../bower_components/ember/ember');
-require('../../bower_components/ember-data/ember-data');
+require('../../bower_components/handlebars/handlebars.js');
+require('../../bower_components/ember/ember.js');
 
-require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-core.max');
-require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-alert.max');
+require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-core.max.js');
+require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-alert.max.js');
 
 var App = Ember.Application.create();
-App.Store = require('./store');
 
 module.exports = App;
 

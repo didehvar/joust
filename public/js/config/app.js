bec24950d99facc60e72b837635e06d232b6c15d
James Didehvar <james@didehvar.com>
2014-04-22 17:01:21 +0100

    use bower dependancies

--- public/js/config/app.js
+++ public/js/config/app.js
@@ -1,10 +1,14 @@
 // require other, dependencies here, ie:
 // require('./vendor/moment');
 
-require('../vendor/jquery');
-require('../vendor/handlebars');
-require('../vendor/ember');
-require('../vendor/ember-data'); // delete if you don't want ember-data
+require('../../bower_components/jquery/dist/jquery');
+
+require('../../bower_components/handlebars/handlebars');
+require('../../bower_components/ember/ember');
+require('../../bower_components/ember-data/ember-data');
+
+require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-core.max');
+require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-alert.max');
 
 var App = Ember.Application.create();
 App.Store = require('./store'); // delete if you don't want ember-data

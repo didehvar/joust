36c09f560d98340458b61a5a69a18d31dec191bb
James Didehvar <james@didehvar.com>
2014-04-22 17:02:03 +0100

    remove pointless comment

--- public/js/config/app.js
+++ public/js/config/app.js
@@ -8,7 +8,7 @@ require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-core.max');
 require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-alert.max');
 
 var App = Ember.Application.create();
-App.Store = require('./store'); // delete if you don't want ember-data
+App.Store = require('./store');
 
 module.exports = App;
 

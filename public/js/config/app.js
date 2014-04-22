c796b22fb1f4c04e15b32b5b950d7f4ffd209507
James Didehvar <james@didehvar.com>
2014-04-22 17:49:26 +0100

    ember now working

--- public/js/config/app.js
+++ public/js/config/app.js
@@ -1,12 +1,4 @@
-require('../../bower_components/jquery/dist/jquery.js');
-
-require('../../bower_components/handlebars/handlebars.js');
-require('../../bower_components/ember/ember.js');
-
-require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-core.max.js');
-require('../../bower_components/ember-addons.bs_for_ember/dist/js/bs-alert.max.js');
 
 var App = Ember.Application.create();
 
 module.exports = App;
-

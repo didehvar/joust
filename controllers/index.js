d0bc7aff5f21b97b394128deb7a723f7ff83f032
James Didehvar <james@didehvar.com>
2014-04-27 23:23:11 +0100

    refactor index controller

--- controllers/index.js
+++ controllers/index.js
@@ -8,7 +8,8 @@
 /** Removes www. from the URL if set. */
 exports.fix_www = function(req, res, next) {
   if (req.headers.host.match(/^www\./)) {
-    return res.redirect(req.protocol + '://' + req.headers.host.slice(4) + req.url, 301);
+    return res.redirect(req.protocol + '://' + req.headers.host.slice(4) +
+      req.url, 301);
   }
 
   next();

d80b5c154c1d802a442e62fe2edb8b807d48ebd8
James Didehvar <james@didehvar.com>
2014-08-09 12:58:39 +0100

    only show first paragraph of news

--- client/views/news/news.js
+++ client/views/news/news.js
@@ -44,60 +44,10 @@ Template.newsPost.createdMoment = function() {
   return moment(this.created_at).format('D MMM YYYY');
 }
 
-// Template.newsPost.created = function() {
-//   if (!this.data || !this.data.id || !this.data.slug) {
-//     return console.log('id or slug missing for topic');
-//   }
-//
-//   var replies = this.data.posts_count;
-//
-//   Meteor.call('discourseGetPost', this.data.id, this.data.slug, this.data.title,
-//               function(error, result) {
-//     if (error) {
-//       return console.log(error);
-//     }
-//
-//     console.log(result);
-//
-//     var id = 'discourse' + this.id;
-//
-//     Session.set(id + 'title', result.title);
-//     Session.set(id + 'content', result.cooked);
-//
-//     Session.set(id + 'link', Meteor.settings.public.discourseUrl + '/t/' +
-//                              result.topic_slug + '/' + result.topic_id);
-//
-//     $('.news-post-' + result.topic_id + ' .news-title').html(
-//       '<a href="' + discourseLink  + '">' +
-//         result.title +
-//       '</a>'
-//     );
-//     $('.news-post-' + result.topic_id + ' .news-content').html(result.cooked);
-//
-//     $('.news-post-' + result.topic_id + ' .news-discourse').html(
-//       '<a href="' + discourseLink  + '">' +
-//         'Join the discussion (' + replies + ' replies)' +
-//       '</a>'
-//     );
-//   });
-// }
-//
-// Template.newsPost.id = function() {
-//   if (!this || !this.id) {
-//     return console.log('id missing');
-//   }
-//
-//   return this.id;
-// }
-//
-// Template.newsPost.title = function() {
-//   return Session.get('discourse' + this.id + 'title');
-// }
-//
-// Template.newsPost.content = function() {
-//   return Session.get('discourse' + this.id + 'content');
-// }
-//
-// Template.newsPost.link = function() {
-//   return Session.get('discourse' + this.id + 'link');
-// }
+Template.newsPost.content = function() {
+  if (!this.cooked) {
+    return;
+  }
+
+  return this.cooked.match(/<p>(.*)<\/p>/)[0];
+}
